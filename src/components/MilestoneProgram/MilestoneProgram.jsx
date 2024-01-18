a8e19212de84a84e7bf9be77c03f7eba155418a3
theme-march <secure.storerepublic@gmail.com>
2024-01-18 12:14:14 +0600

    kjhgjdfjg

--- src/components/MilestoneProgram/MilestoneProgram.jsx
+++ src/components/MilestoneProgram/MilestoneProgram.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { ButtonMore } from "../Button/Button";
 
 export default function MilestoneProgram({ props }) {
+  const { details, title, url } = props.details;
   return (
     <div className="container">
       <div className="row align-items-center justify-content-between gap-5">
         <div className="col-lg-5">
           <div>
-            <h1>{props?.title}</h1>
+            <h1>{title}</h1>
             <div className="ak-height-25 ak-height-lg-20"></div>
-            <p dangerouslySetInnerHTML={{ __html: props?.details.details }} />
+            <p dangerouslySetInnerHTML={{ __html: details }} />
             <div className="ak-height-25 ak-height-lg-20"></div>
-            <ButtonMore to={props?.url}>VIEW MORE</ButtonMore>
+            <ButtonMore to={url}>VIEW MORE</ButtonMore>
           </div>
         </div>
         <div className="col-lg-6">
