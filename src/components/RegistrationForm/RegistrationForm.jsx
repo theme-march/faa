adfd0839cf25c04f6d404d40ec61092268eeb794
theme-march <secure.storerepublic@gmail.com>
2024-02-08 12:19:53 +0600

    occ

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -34,6 +34,7 @@ export default function RegistrationForm() {
     "Service (Private)",
     "Business",
     "Consultant",
+    "Other",
   ];
   const [memberRegister] = useMemberRegisterMutation();
 
