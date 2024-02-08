2972db71d5c8b0b0c1a23fe0a10deb9f1effca06
theme-march <secure.storerepublic@gmail.com>
2024-02-08 12:30:10 +0600

    asdasds

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { toast } from "react-toastify";
-// import DatePicker from "react-datepicker";
+import DatePicker from "react-datepicker";
 // import "react-datepicker/dist/react-datepicker.css";
 import { useMemberRegisterMutation } from "../../features/member/memberApiIn";
 
