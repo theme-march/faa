5486ed6fcce1c646c465af23d0c7b22ca5b40dfe
theme-march <secure.storerepublic@gmail.com>
2024-03-12 15:37:23 +0600

    ug8asd asjdasd

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,13 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3001,
+    host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000",
+        target: "http://127.0.0.1:3000/api",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
