85b88f2329b6760e62729857f9519f849112d882
tareq <tareq236@gmail.com>
2024-12-30 14:42:03 +0600

    modify donation payment

--- src/components/EventRegistrationForm/EventParticipateRegistrationForm.jsx
+++ src/components/EventRegistrationForm/EventParticipateRegistrationForm.jsx
@@ -538,21 +538,21 @@ export default function EventParticipateRegistrationForm({ props: eventId }) {
                 />
                 {deliveryOption === "from_home" ? (
                   <>
-                    <InputField
-                       label="Delivery Charge"
-                       id="deliveryCharge"
-                       type="select"
-                       register={register}
-                       field="delivery_charge"
-                       validation={{required: true}}
-                       options={[
-                           {value: "60", label: "For Dhaka (60 TK)"},
-                           {value: "110", label: "Outside Dhaka (110 TK)"},
-                       ]}
-                       onChange={(e) => handleDeliveryChargeChange(e.target.value)}
-                       errorMessage={{required: "Please select a delivery charge"}}
-                       error={errors.delivery_charge}
-                    />
+                    {/*<InputField*/}
+                    {/*   label="Delivery Charge"*/}
+                    {/*   id="deliveryCharge"*/}
+                    {/*   type="select"*/}
+                    {/*   register={register}*/}
+                    {/*   field="delivery_charge"*/}
+                    {/*   validation={{required: true}}*/}
+                    {/*   options={[*/}
+                    {/*       {value: "60", label: "For Dhaka (60 TK)"},*/}
+                    {/*       {value: "110", label: "Outside Dhaka (110 TK)"},*/}
+                    {/*   ]}*/}
+                    {/*   onChange={(e) => handleDeliveryChargeChange(e.target.value)}*/}
+                    {/*   errorMessage={{required: "Please select a delivery charge"}}*/}
+                    {/*   error={errors.delivery_charge}*/}
+                    {/*/>*/}
                     <div style={{ fontSize: 12 }}>
                       <div style={{ fontWeight: "bold" }}>Delivery Address</div>
                       <div>
