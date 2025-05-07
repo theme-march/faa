be349af10edfbbbab4311c238394783bf675ef58
akashlil <akashlil393@gmail.com>
2025-05-07 21:45:04 +0600

    update gallery url

--- src/components/GalleryGrid/GalleryItem.jsx
+++ src/components/GalleryGrid/GalleryItem.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 export default function GalleryItem({ props }) {
   return (
     <Link
-      to={`http://139.162.11.50:3000/gallery_image/${props}`}
+      to={`${props}`}
       className={`col`}
     >
       <img
-        src={`http://139.162.11.50:3000/gallery_image/${props}`}
-        // alt={gallery.image}
+      src={`/images/gallery_image/${props}`}
+        alt={props}
       />
     </Link>
   );
